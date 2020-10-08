e4de74f3919d747d580f63cb3544679748160c25
sarahbarron <sarahflavin@yahoo.com>
2020-10-08 11:42:48 +0100

    models package added with title

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -1,20 +1,32 @@
 package org.wit.hillfort.activities
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.wit.hillforts.R
 import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.toast
+import org.wit.hillfort.models.HillfortModel
+import kotlinx.android.synthetic.main.activity_hillfort.hillfortTitle
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+    var hillfort = HillfortModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         info("Hillforts activity started...")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
 
         btnAdd.setOnClickListener() {
-            info("add Button Pressed")
+            hillfort.title = hillfortTitle.text.toString()
+            if(hillfort.title.isNotEmpty()){
+                info("Add button pressed: $hillfort")
+            }
+            else{
+                toast("Please Enter a title")
+            }
+
         }
     }
 
