59b86717a2b8a93f64748f3212c5902328b9763f
sarahbarron <sarahflavin@yahoo.com>
2020-10-08 10:16:16 +0100

    TextView with title

--- app/src/main/java/org/wit/hillforts/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/HillfortActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.hillforts.R
+import kotlinx.android.synthetic.main.activity_hillfort.*
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         info("Hillforts activity started...")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+
+
     }
+
+
 }
