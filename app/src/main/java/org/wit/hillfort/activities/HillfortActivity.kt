7ee60155ec2ec6ef08bbb938f23e0a8ccc8101e1
sarahbarron <sarahflavin@yahoo.com>
2020-10-08 12:43:44 +0100

    used lateinit to overrule the null safety checks on MainApp

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -13,23 +13,24 @@ import org.wit.hillfort.main.MainApp
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     var hillfort = HillfortModel()
-//    val hillforts = ArrayList<HillfortModel>()
-    var app : MainApp? = null
+
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         info("Hillforts activity started...")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
             hillfort.title = hillfortTitle.text.toString()
             hillfort.description = description.text.toString()
 
             if(hillfort.title.isNotEmpty()){
-                app!!.hillforts.add(hillfort.copy())
+                app.hillforts.add(hillfort.copy())
                 info("Add button pressed: $hillfort")
-                for(i in app!!.hillforts.indices){
-                    info("Hillfort[$i]:${app!!.hillforts[i]}")
+                for(i in app.hillforts.indices){
+                    info("Hillfort[$i]:${app.hillforts[i]}")
                 }
             }
             else{
