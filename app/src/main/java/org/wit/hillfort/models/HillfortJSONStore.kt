4766a817d31c01e1cf2b59f948bbda230d14a709
sarahbarron <sarahflavin@yahoo.com>
2020-10-23 14:38:01 +0100

    JSON store added to create hillfortsand find all hillforts to and from a JSON file. Methods to serialize and deserialize data added to complete these functions

---
+++ app/src/main/java/org/wit/hillfort/models/HillfortJSONStore.kt
@@ -0,0 +1,61 @@
+package org.wit.hillfort.models
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.jetbrains.anko.AnkoLogger
+import org.wit.hillfort.helpers.*
+import java.util.*
+
+val JSON_FILE = "hillforts.json"
+val gsonBuilder = GsonBuilder().setPrettyPrinting().create()
+val listType = object : TypeToken<java.util.ArrayList<HillfortModel>>() {}.type
+
+// Generate a random ID for each hillfort
+fun generateRandomId(): Long {
+    return Random().nextLong()
+}
+
+
+class HillfortJSONStore : HillfortStore, AnkoLogger {
+
+    val context: Context
+    var hillforts = mutableListOf<HillfortModel>()
+
+//    Constructor checks if the file exists if it does it deserializes it
+    constructor (context: Context) {
+        this.context = context
+        if (exists(context, JSON_FILE)) {
+            deserialize()
+        }
+    }
+
+//    Return a list of all hillforts
+    override fun findAll(): MutableList<HillfortModel> {
+        return hillforts
+    }
+
+//    Create a hillfort
+    override fun create(hillfort: HillfortModel) {
+        hillfort.id = generateRandomId()
+        hillforts.add(hillfort)
+        serialize()
+    }
+
+// Update a hillfort
+    override fun update(hillfort: HillfortModel) {
+        // todo
+    }
+// Serialize / write data to the JSON file
+    private fun serialize() {
+        val jsonString = gsonBuilder.toJson(hillforts, listType)
+        write(context, JSON_FILE, jsonString)
+    }
+
+//    deserialize / read data from the JSON file
+    private fun deserialize() {
+        val jsonString = read(context, JSON_FILE)
+        hillforts = Gson().fromJson(jsonString, listType)
+    }
+}
