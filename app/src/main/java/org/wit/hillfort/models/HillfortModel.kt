fd6d28d2f0588695d67b19282c0cf4e3fce63da7
sarahbarron <sarahflavin@yahoo.com>
2020-10-23 13:16:03 +0100

    update the hillfort modal to include the latitude, longitude and zoom data and include the values when a hillfort is updated

--- app/src/main/java/org/wit/hillfort/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillfort/models/HillfortModel.kt
@@ -6,7 +6,11 @@ import kotlinx.android.parcel.Parcelize
 data class HillfortModel(var id: Long = 0,
                          var name: String = "",
                          var description: String="",
-                         var image: String=""): Parcelable
+                         var image: String="",
+                         var lat: Double = 0.0,
+                         var lng: Double = 0.0,
+                         var zoom: Float = 0f
+                         ): Parcelable
 
 @Parcelize
 data class Location(var lat: Double = 0.0,
