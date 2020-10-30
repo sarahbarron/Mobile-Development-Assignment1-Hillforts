8c9bb97a7edba8dde6b12565b6afd19332b2a5d2
sarahbarron <sarahflavin@yahoo.com>
2020-10-30 20:43:39 +0000

    empty user helper functions

---
+++ app/src/main/java/org/wit/hillfort/helpers/UserHelpers.kt
@@ -0,0 +1,24 @@
+package org.wit.hillfort.helpers
+
+fun findAll(){
+}
+
+fun findOne(username: String){
+
+}
+
+fun create(username:String, password:String){
+
+}
+
+fun deleteAll(){
+
+}
+
+fun deleteOne(username:String){
+
+}
+
+fun authenticate(username: String, password: String){
+
+}
